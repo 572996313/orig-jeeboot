55525eb7b82e2a5e26e673bb5457bb81f2b25028
T\linux <linuxdo_llllxf@outlook.com>
2025-11-16 20:22:26 +0800

    feat: 重构敏感字段相关类至core模块

--- jeecg-boot-base-core/src/main/java/org/jeecg/common/api/CommonAPI.java
+++ jeecg-boot-base-core/src/main/java/org/jeecg/common/api/CommonAPI.java
@@ -1,12 +1,11 @@
 package org.jeecg.common.api;
 
 import org.jeecg.common.api.dto.AiragFlowDTO;
+import org.jeecg.common.system.vo.*;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
-import org.jeecg.common.system.vo.*;
 
 /**
  * 通用api
